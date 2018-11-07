9146f24524097cc600173200624ea5ac7bbc7e6b
KeitaKishino <peposhi.keita@gmail.com>
2018-11-07 11:24:58 +0900

    improve fizzbuzz.rb from make_fizzbuzz

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,14 @@
 100.times{|i|
 	if i % 3 == 0 then
 		print "fizz"
+	end
 	if i % 5 == 0 then
 		print "buzz"
+	end
 	if i % 7 == 0 then
 		print "git"
-	else
+	end
+	unless i % 3 == 0 && i % 5 == 0 && i % 7 == 0 then
 		print i
 	end
 	
